cfbd44ccde72f4ceb148c2c67b14a34a0c00a4f5
Jacques Fourie <jfourie@madisoncollege.edu>
2018-03-15 19:47:29 -0500

    Adding tests to TemperatureConversionsTest.java

--- src/test/java/com/daehosting/TemperatureConversionsTest.java
+++ src/test/java/com/daehosting/TemperatureConversionsTest.java
@@ -9,21 +9,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class TemperatureConversionsTest {
 
     @Test
-    void getTemperatureConversionsSoap() {
-    }
-
-    @Test
-    void getTemperatureConversionsSoap1() {
-    }
-
-    @Test
-    void getTemperatureConversionsSoap12() {
+    void celsiusToFahrenheitTest() {
         TemperatureConversionsSoapType service = new TemperatureConversions().getTemperatureConversionsSoap12();
         BigDecimal result = service.celsiusToFahrenheit(BigDecimal.valueOf(0));
         assertEquals(BigDecimal.valueOf(32), result);
     }
 
     @Test
-    void getTemperatureConversionsSoap121() {
+    void fahrenheitToCelsiusTest() {
+        TemperatureConversionsSoapType service = new TemperatureConversions().getTemperatureConversionsSoap12();
+        BigDecimal result = service.fahrenheitToCelsius(BigDecimal.valueOf(40));
+        assertEquals(BigDecimal.valueOf(4.4444444), result);
     }
+
+    @Test
+    void windChillInCelsiusTest() {
+        TemperatureConversionsSoapType service = new TemperatureConversions().getTemperatureConversionsSoap12();
+        BigDecimal result = service.windChillInCelsius(BigDecimal.valueOf(15), BigDecimal.valueOf(4));
+        assertEquals(BigDecimal.valueOf(12.7704), result);
+    }
+
+    @Test
+    void windChillInFahrenheitTest() {
+        TemperatureConversionsSoapType service = new TemperatureConversions().getTemperatureConversionsSoap12();
+        BigDecimal result = service.windChillInFahrenheit(BigDecimal.valueOf(15), BigDecimal.valueOf(4));
+        assertEquals(BigDecimal.valueOf(8.61072017), result);
+    }
+
 }
